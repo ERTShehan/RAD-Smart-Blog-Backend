17992c1a8cc81877da20148c542a9be18a76c989
Shehan <ertsshehan@gmail.com>
2025-11-20 13:45:06 +0530

    Add CORS support for Vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://rad-smart-blog-backend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"] // optional
   })
 )
